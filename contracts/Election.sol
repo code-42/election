5df4254406f5a98e9e1abb79b990594ff1bfd230
Edward Dupre <edwd42@gmail.com>
2018-11-24 14:39:56 -0500

    declare struct Candidate{}

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,7 +2,15 @@ pragma solidity 0.4.24;
 
 contract Election {
     // Read/write candidate
-    string public candidate;
+    // string public candidate;
+
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
 
     // Constructor
     constructor () public {
